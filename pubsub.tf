9548ab66916a361ef565b67517a374bca27bbc0c
Asad Ullah Khan <asad.ullah.khan@macys.com>
2024-01-23 18:46:11 +0500

    add pubsub

---
+++ pubsub.tf
@@ -0,0 +1,14 @@
+resource "google_pubsub_topic" "B_MSP_LTY_MB_MONEY_RC_GCP_SEND-topic" {
+  name = "B.MSP.LTY.MB.MONEY.RC.GCP.SEND"
+}
+
+resource "google_pubsub_subscription" "B_MSP_LTY_MB_MONEY_RC_GCP_SEND_SUB" {
+  depends_on = [
+    google_pubsub_topic.B_MSP_LTY_MB_MONEY_RC_GCP_SEND-topic
+  ]
+  name  = "B.MSP.LTY.MB.MONEY.RC.GCP.RECV"
+  topic = google_pubsub_topic.B_MSP_LTY_MB_MONEY_RC_GCP_SEND-topic.id
+  expiration_policy {
+    ttl = "31536000s"
+  }
+}
