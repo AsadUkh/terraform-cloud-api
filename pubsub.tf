5f2b651ebea617a05f831b7f5c373e1708546b4a
Asad Ullah Khan <asad.ullah.khan@macys.com>
2024-02-02 14:38:19 +0500

    checking global vairables

--- pubsub.tf
+++ pubsub.tf
@@ -6,12 +6,3 @@ resource "aws_s3_bucket" "creationofbucket" {
     Environment = "Dev"
   }
 }
-
-resource "aws_s3_bucket" "creationofbucket1" {
-  bucket = "asad-bucket2"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
