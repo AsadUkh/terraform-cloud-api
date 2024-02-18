1e311f1bf1500486279886363ad04a731bfc1ce2
Asad Ullah Khan <asad.ullah.khan@macys.com>
2024-02-18 14:48:49 +0500

    addingg one more bucket github actions

--- pubsub.tf
+++ pubsub.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "creationofbucket" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket" "github-action" {
+  bucket = "githubactions"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
